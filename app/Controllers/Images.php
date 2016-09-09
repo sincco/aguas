db34bac37aa7acf7e3faacdfcd23f75a652eb30f
Ivan Miranda <pa.ivan.miranda@gmail.com>
2016-09-09 14:54:50 -0500

    Avances20160909

---
+++ app/Controllers/Images.php
@@ -0,0 +1,31 @@
+<?php
+
+use \Sincco\Sfphp\Response;
+
+class ImagesController extends Sincco\Sfphp\Abstracts\Controller 
+{
+	public function resize() {
+		$directories = scandir(PATH_ROOT . '/_expedientes');
+		array_shift($directories);
+		array_shift($directories);
+		$adjuntos = [];
+		foreach ($directories as $dir) {
+			$files = scandir(PATH_ROOT . '/_expedientes/' .str_replace(' ', '%20', $dir));
+			array_shift($files);
+			array_shift($files);
+			foreach ($files as $file) {
+				if (!is_dir(PATH_ROOT . '/_expedientes/' . $dir . '/' . $file)) {
+					$fileOld = PATH_ROOT . '/_expedientes/' . $dir . '/' . $file;
+					$fileNew = PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/' . $file;
+					if (!is_dir(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/')) {
+						mkdir(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/');
+						chmod(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/', 0777);
+					}
+					if (!file_exists($fileNew)) {
+						$this->helper('Images')->resize($fileOld, $fileNew);
+					}
+				}
+			}
+		}
+	}
+}
--- app/Expedientes/Controllers/Index.php
+++ app/Expedientes/Controllers/Index.php
@@ -28,29 +28,6 @@ class IndexController extends Sincco\Sfphp\Abstracts\Controller
 		$view->render();
 	}
 
-	public function _resize() {
-		$directories = scandir(PATH_ROOT . '/_expedientes/' . $this->getParams('contrato'));
-		array_shift($directories);
-		array_shift($directories);
-		$adjuntos = [];
-		foreach ($directories as $dir) {
-			$files = scandir(PATH_ROOT . '/_expedientes/' .str_replace(' ', '%20', $dir));
-			array_shift($files);
-			array_shift($files);
-			foreach ($files as $file) {
-				$fileOld = PATH_ROOT . '/_expedientes/' . $dir . '/' . $file;
-				$fileNew = PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/' . $file;
-				if (!is_dir(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/')) {
-					mkdir(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/');
-					chmod(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/', 0777);
-				}
-				if (!file_exists($fileNew)) {
-					$this->helper('Images')->resize($fileOld, $fileNew);
-				}
-			}
-		}
-	}
-
 	public function apiData() {
 		$model = $this->getModel('Expedientes\Contratos');
 		$data = $model->getTable($_GET);
