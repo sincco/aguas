53cbbe02195181e6e4e4373b7c5f4a5937c92a1c
Ivan Miranda <pa.ivan.miranda@gmail.com>
2016-09-09 15:27:22 -0500

    Avances20160909

--- app/Controllers/Images.php
+++ app/Controllers/Images.php
@@ -10,22 +10,21 @@ class ImagesController extends Sincco\Sfphp\Abstracts\Controller
 		array_shift($directories);
 		$adjuntos = [];
 		foreach ($directories as $dir) {
-			if (!is_dir(PATH_ROOT . '/_expedientes/' .str_replace(' ', '%20', $dir))) {
-				continue;
-			}
-			$files = scandir(PATH_ROOT . '/_expedientes/' .str_replace(' ', '%20', $dir));
-			array_shift($files);
-			array_shift($files);
-			foreach ($files as $file) {
-				if (strpos($file, '_thumbnails') === FALSE) {
-					$fileOld = PATH_ROOT . '/_expedientes/' . $dir . '/' . $file;
-					$fileNew = PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/' . $file;
-					if (!is_dir(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/')) {
-						mkdir(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/');
-						chmod(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/', 0777);
-					}
-					if (!file_exists($fileNew)) {
-						$this->helper('Images')->resize($fileOld, $fileNew);
+			if (is_dir(PATH_ROOT . '/_expedientes/' .str_replace(' ', '%20', $dir))) {
+				$files = scandir(PATH_ROOT . '/_expedientes/' .str_replace(' ', '%20', $dir));
+				array_shift($files);
+				array_shift($files);
+				foreach ($files as $file) {
+					if (strpos($file, '_thumbnails') === FALSE) {
+						$fileOld = PATH_ROOT . '/_expedientes/' . $dir . '/' . $file;
+						$fileNew = PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/' . $file;
+						if (!is_dir(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/')) {
+							mkdir(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/');
+							chmod(PATH_ROOT . '/_expedientes/' . $dir . '/_thumbnails/', 0777);
+						}
+						if (!file_exists($fileNew)) {
+							$this->helper('Images')->resize($fileOld, $fileNew);
+						}
 					}
 				}
 			}
