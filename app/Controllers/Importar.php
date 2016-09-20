92e1ee6dd8fe3ce25be1476b4f67311e88c817f6
Ivan Miranda <ivan.miranda@sincco.com>
2016-09-19 19:26:57 -0500

    Avances20160919

--- app/Controllers/Importar.php
+++ app/Controllers/Importar.php
@@ -14,10 +14,10 @@ class ImportarController extends Sincco\Sfphp\Abstracts\Controller {
 			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 		} else {
 			$stid = oci_parse($conn, "SELECT NIS, to_char(ALTACONTRATO, 'yyyy-mm-dd') ALTACONTRATO, PROPIETARIO, USUARIO, SUMINISTRO, NUMTOMAS, GIRO, UTILIZACION, TARIFA, SERVICIOS, NIVELTARIFARIO, ASOCIACION, CVECATASTRAL, MUNICIPIO, COLONIA, VIA, CALLE, NUMOFICIAL, INTERIOR_1, INTERIOR_2, MARCA_ACT, NUM_APA, APARATO, TIP_MEDICION, F_INST, ANOS_DISP_MED, MAYORES_5_ANOS, to_char(F_RETIRO_DISP_MED, 'yyyy-mm-dd') F_RETIRO_DISP_MED FROM PROVEXTERN.PADRON_COLONIAS_MEDIDORES WHERE ROWNUM <= 5");
+			oci_execute($stid);
 			oci_fetch_all($stid, $contratos);
 			$contratos = count($contratos);
 			$contrato = 1;
-			oci_execute($stid);
 			while ($padron = oci_fetch_assoc($stid)) {
 				echo "Procesando " . $contrato . " de " . $contratos . "<br>\n";
 				$query = "INSERT INTO contratos (contrato, altaContrato, propietario, usuario, suministro, numTomas, giro, utilizacion, tarifa, servicios, nivelTarifario, asociacion, cveCatastral, municipio, colonia, via, calle, numOficial, longitud, latitud, serieMedidor, telemetriaMedidor, interior, interior2, marca_act, num_apa, aparato, tip_medicion, f_inst, anos_disp_med, mayores_5_anos, f_retiro_disp_med, cobro, mod_med_ins, tipo_material, caudal_max, num_dig_medidor, f_fabricacion, diametro_toma, lectura_inicial)
