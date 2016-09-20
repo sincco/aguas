41f6a0d822aeda6a14d8d03a9b11e2a15f5b806a
Ivan Miranda <ivan.miranda@sincco.com>
2016-09-19 20:11:08 -0500

    Avances20160919

--- app/Controllers/Importar.php
+++ app/Controllers/Importar.php
@@ -14,7 +14,7 @@ class ImportarController extends Sincco\Sfphp\Abstracts\Controller {
 			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 		} else {
 			$modelo = $this->getModel('Aguas');
-			$contratos = $modelo->query('SELECT GROUP_CONCAT(contrato) contratos FROM contratos');
+			$contratos = $modelo->execute('SELECT GROUP_CONCAT(contrato) contratos FROM contratos');
 			var_dump($contratos);
 			die();
 			$stid = oci_parse($conn, "SELECT NIS, to_char(ALTACONTRATO, 'yyyy-mm-dd') ALTACONTRATO, PROPIETARIO, USUARIO, SUMINISTRO, NUMTOMAS, GIRO, UTILIZACION, TARIFA, SERVICIOS, NIVELTARIFARIO, ASOCIACION, CVECATASTRAL, MUNICIPIO, COLONIA, VIA, CALLE, NUMOFICIAL, INTERIOR_1, INTERIOR_2, MARCA_ACT, NUM_APA, APARATO, TIP_MEDICION, F_INST, ANOS_DISP_MED, MAYORES_5_ANOS, to_char(F_RETIRO_DISP_MED, 'yyyy-mm-dd') F_RETIRO_DISP_MED FROM PROVEXTERN.PADRON_COLONIAS_MEDIDORES WHERE ROWNUM <= 5");
