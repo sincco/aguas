7e14e994936bde16beb10337d43fb5114ee9f033
Iván Miranda <ivan@pengostores.com>
2016-09-30 13:24:24 -0500

    Avances20160930

--- app/Controllers/Importar.php
+++ app/Controllers/Importar.php
@@ -20,6 +20,9 @@ class ImportarController extends Sincco\Sfphp\Abstracts\Controller {
 				if (trim($padron["F_RETIRO_DISP_MED"]) == '') {
 					$padron["F_RETIRO_DISP_MED"] = '1900-01-01';
 				}
+				if (trim($padron["F_INST"]) == '') {
+					$padron["F_INST"] = '1900-01-01';
+				}
 				echo "<br>------Procesando " . $padron["NIS"] . "<br>\n";
 				var_dump($padron);
 				$query = "INSERT INTO contratos (contrato, altaContrato, propietario, usuario, suministro, numTomas, giro, utilizacion, tarifa, servicios, nivelTarifario, asociacion, cveCatastral, municipio, colonia, via, calle, numOficial, longitud, latitud, serieMedidor, telemetriaMedidor, interior, interior2, marca_act, num_apa, aparato, tip_medicion, f_inst, anos_disp_med, mayores_5_anos, f_retiro_disp_med, cobro, mod_med_ins, tipo_material, caudal_max, num_dig_medidor, f_fabricacion, diametro_toma, lectura_inicial, region, estrato, bandera)
