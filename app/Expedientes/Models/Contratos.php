5785079da4ab160a194a63af39a73879b1592824
Iván Miranda <pa.ivan.miranda@gmail.com>
2016-11-08 18:12:02 -0600

    Avances20161108

--- app/Expedientes/Models/Contratos.php
+++ app/Expedientes/Models/Contratos.php
@@ -28,7 +28,7 @@ class ContratosModel extends Sincco\Sfphp\Abstracts\Model {
 	}
 
 	public function getTable($data, $cuadrilla = 0) {
-		ini_set("memory_limit", "128M");
+		ini_set("memory_limit", "256M");
 		$modCuadrilla = '';
 		if (!isset($data['sort'])) {
 			$data['sort'] = 'contrato';
