027bbb63cede0bc829cf551ed47fed99a2150e6e
Iván Miranda <ivan.miranda@sincco.com>
2016-11-17 17:59:00 -0600

    Avances20161117

--- index.php
+++ index.php
@@ -1,32 +1,5 @@
 <?php
-# NOTICE OF LICENSE
-#
-# This source file is subject to the Open Software License (OSL 3.0)
-# that is available through the world-wide-web at this URL:
-# http://opensource.org/licenses/osl-3.0.php
-#
-# -----------------------
-# @author: Iván Miranda (@deivanmiranda)
-# @version: 1.0.0
-# -----------------------
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/paths.php';
-
-use Sincco\Sfphp\Session;
-use Sincco\Sfphp\Launcher;
-use Sincco\Tools\Debug;
-use Sincco\Sfphp\Translations;
-
-try {
-	Translations::init();
-	Session::get();
-	new Launcher();
-}catch (\Exception $err) {
-	$errorInfo = sprintf( '%s: %s in %s on line %s.',
-		'Error',
-		$err,
-		$err->getFile(),
-		$err->getLine()
-	);
-	Debug::dump( $errorInfo );
-}
+define('PATH_ROOT', __DIR__);
+$app = new Sincco\Sfphp\App;
+$app->run();
