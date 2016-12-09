f7c67f7b3f6baab507e1efd812d7d136f2cce29e
Iván Miranda <ivan.miranda@sincco.com>
2016-12-08 19:04:21 -0600

    Avances20161208

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -1,9 +1,10 @@
 <?php
 
 use \Sincco\Sfphp\Config\Reader;
-use \Sincco\Sfphp\Request;
 use \Sincco\Sfphp\Crypt;
+use \Sincco\Sfphp\Request;
 use \Sincco\Tools\Login;
+use \Sincco\Tools\Tokenizer;
 
 /**
  * Control de acceso al sistema
@@ -39,33 +40,18 @@ class LoginController extends Sincco\Sfphp\Abstracts\Controller {
 	 * @return none
 	 */
 	public function apiLogin() {
+		$acceso = FALSE;
 		$db = Reader::get('bases');
 		$db = $db['default'];
 		$db['password'] = trim(Crypt::decrypt($db['password']));
 		Login::setDatabase($db);
-		if (Login::login(Request::getParams('userData'))) {
-			$cuadrilla = $this->getModel('Catalogos\Usuarios')->getByUserName(Request::getParams('userData')['user']);
-			$cuadrilla = array_pop($cuadrilla);
-			$_SESSION['user\extra'] = serialize(['cuadrilla'=>$cuadrilla]);
-			$data = unserialize($_SESSION['sincco\login\controller']);
-			$_SESSION['user\vendedor'] = $this->getModel('Catalogos\Usuarios')->esVendedor(['userId'=>$data['userId']])[0]['vendedor'];
-			$acceso = TRUE;
-		}
-		else {
-			$acceso = FALSE;
+		$data = $this->getParams('userData');
+		$token = Tokenizer::validate($data['_token'], APP_KEY);
+		if(isset($token['GENERIC_API'])) {
+			if(Login::login($data)) {
+				$acceso = TRUE;
+			}
 		}
 		echo json_encode(array('acceso'=>$acceso));
-	}
-
-	/**
-	 * Crea una nueva cuenta de usuario
-	 * @return none
-	 */
-	public function apiRegisterUser() {
-		$db = Reader::get('bases');
-		$db = $db['default'];
-		$db['password'] = trim(Crypt::decrypt($db['password']));
-		Login::setDatabase($db);
-		var_dump(Login::createUser(array('user'=>'ivan', 'email'=>'ivan', 'password'=>'eco123')));
 	}
 }
