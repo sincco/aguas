9ef1db3bf008bee1fa1393ec9b4a69a5cdecbbb5
Iván Miranda <ivan.miranda@sincco.com>
2017-01-04 10:29:56 -0600

    Avances20170104

--- app/Helpers/UsersAccount.php
+++ app/Helpers/UsersAccount.php
@@ -80,6 +80,13 @@ class UsersAccountHelper extends Sincco\Sfphp\Abstracts\Helper {
 			array_shift($response[0]['childs']);
 			array_shift($response[0]['childs']);
 		}
+		$cuadrilla = unserialize($_SESSION['user\extra']);
+		if(isset($cuadrilla['cuadrilla']['cuadrilla'])) {
+			array_shift($response);
+			array_shift($response);
+			array_shift($response[1]['childs']);
+			array_shift($response[1]['childs']);
+		}
 		return $response;
 	}
 
