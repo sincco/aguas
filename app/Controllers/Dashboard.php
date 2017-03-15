2d3f29561a100d86b74b6ad34f73dc5c0ef943dc
Ivan Miranda <ivan@sincco.com}>
2017-03-15 16:33:11 -0600

    Visor

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -16,7 +16,7 @@ class DashboardController extends Sincco\Sfphp\Abstracts\Controller {
 	public function index() {
 		$user = unserialize($_SESSION['sincco\login\controller']);
 		var_dump(stripos($user['userName'], "region"));
-		if (stripos($user['userName'], "region")) {
+		if (stripos($user['userName'], "region") !== false) {
 			Request::redirect('gestion/visor');
 		}
 		$this->helper('UsersAccount')->checkLogin();
