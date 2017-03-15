9738c8699fc0f406f60dae0532baaf8d5ea7c9a4
Ivan Miranda <ivan@sincco.com}>
2017-03-15 16:40:46 -0600

    Visor

--- app/Api/V2/Controllers/Expedientes.php
+++ app/Api/V2/Controllers/Expedientes.php
@@ -13,4 +13,26 @@ class ExpedientesController extends Sincco\Sfphp\Abstracts\Controller
 		$model = $this->getModel('Aguas');
 		new Response('json', ['data'=>$model->contratosImages()->where('contrato', $this->getParams('contrato'))->getData()]);
 	}
+
+	public function resumen() {
+		$pagination = [0];
+        $contratos = $this->getModel('Expedientes\Contratos');
+        switch (Request::get('method')) {
+            case 'GET':
+                if (isset($_GET['pagination'])) {
+                    $pagination = $_GET['pagination'];
+                }
+                $filters = [];
+                if (isset($_GET['filters'])) {
+                    $filters = $_GET['filters'];
+                }
+                $data = $contratos->getDataFiltered($filters, $pagination);
+                $total = $contratos->getTotalDataFiltered($filters);
+                new Response('json', ['data'=>$data, 'registros'=>$total, 'extra'=>'']);
+                break;
+            default:
+                new Response('json', ['data'=>false, 'extra'=>'Método ' . Request::get('method') . ' no soportado']);
+                break;
+        }
+	}
 }
