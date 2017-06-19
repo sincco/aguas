c46e68ac629d44b36450b0a53836c78ce828dfae
Iván Miranda <ivan.miranda@sincco.com>
2017-06-19 18:40:44 -0500

    Avances20170619

--- app/Expedientes/Controllers/Index.php
+++ app/Expedientes/Controllers/Index.php
@@ -123,8 +123,12 @@ class IndexController extends Sincco\Sfphp\Abstracts\Controller
 		$cuadrilla = $this->getParams('cuadrilla');
 		$model = $this->getModel('Expedientes\Contratos');
 		$data = $model->getTable($_GET, $cuadrilla);
-		$count = 125000; #$model->getCount($_GET, $cuadrilla);
-		new Response('json', ['total'=>$count[0]['total'], 'rows'=>$data]);
+		if (count($data) > 100) {
+			$count = 127486; #$model->getCount($_GET);
+		} else {
+			$count = count($data);
+		}
+		new Response('json', ['total'=>$count, 'rows'=>$data]);
 	}
 
 	public function apiAsignar() {
