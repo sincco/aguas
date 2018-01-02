5a4a9446bdec85e106ce023fbf4f767c87f7ffa0
Iván Miranda <pa.ivan.miranda@gmail.com>
2018-01-02 11:20:29 -0600

    Update Importar.php

--- app/Controllers/Importar.php
+++ app/Controllers/Importar.php
@@ -19,6 +19,9 @@ class ImportarController extends Sincco\Sfphp\Abstracts\Controller {
 			$total += count($data);
 			$modelo = $this->getModel('Aguas');
 			foreach ($data as $row) {
+				if (!isset($row['INTERIOR']) {
+					$row['INTERIOR'] = '';
+				}
 				$query = "INSERT INTO `contratos`(`contrato`,  `propietario`, `usuario`,  `municipio`, `region`, `via`, `calle`, `numOficial`, `interior`, `colonia`) VALUES ('" . $row["NIS"] . "','" . $row["PROPIETARIO"] . "','" . str_replace("'", '', $row["USUARIO"]) . "','" . str_replace("'", '', $row["MUNICIPIO"]) . "','" . str_replace("'", '', $row["REGION"]) . "','" . str_replace("'", '', $row["VIA"]) . "','" . str_replace("'", '', $row["CALLE"]) . "','" . str_replace("'", '', $row["NUM_OFICIAL"]) . "','" . str_replace("'", '', $row["INTERIOR"]) . "','" . str_replace("'", '', $row["COLONIA"]) . "';";
 				var_dump($query);
 				#$modelo->execute($query);
