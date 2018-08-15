a512e26fb4e8642d50d070484718458b72c24667
Iván Miranda <pa.ivan.miranda@gmail.com>
2018-08-15 18:28:58 -0500

    Update Empresas.php

--- app/Catalogos/Controllers/Empresas.php
+++ app/Catalogos/Controllers/Empresas.php
@@ -1,1 +1,19 @@
 
+<?php
+use \Sincco\Sfphp\Response;
+class EmpresasController extends Sincco\Sfphp\Abstracts\Controller {
+	public function index() {
+		$this->helper('UsersAccount')->checkLogin();
+		$model = $this->getModel('Catalogos\Empresas');
+		$view = $this->newView('Catalogos\EmpresasTabla');
+		$view->cuadrillas = $model->getAll();
+		$view->menus = $this->helper('UsersAccount')->createMenus();
+		$view->render();
+	}
+	public function api() {
+		$cuadrilla = $this->getParams('empresa');
+		$model = $this->getModel('Catalogos\Empresas');
+		$cuadrillaId = $model->insert($empresa);
+		new Response('json', ['respuesta'=>$cuadrillaId]);
+	}
+}
