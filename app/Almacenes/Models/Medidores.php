5452e7513b66a5d7d699ba4571ce83357ac6e826
Your Name <you@example.com>
2018-09-08 01:21:04 -0500

    Merge

--- app/Almacenes/Models/Medidores.php
+++ app/Almacenes/Models/Medidores.php
@@ -9,7 +9,8 @@ class MedidoresModel extends Sincco\Sfphp\Abstracts\Model
 
 	public function setRegister($data)
 	{
-		$query = "SELECT * FROM medidores WHERE serie = '" . $data['serie'] . "';";
+			$query = "SELECT m.serie,m.diametro,m.modelo,e.descripcion,m.marca,m.fechaAsignacion FROM medidores m
+			inner join empresas e on m.empresaId=e.idEmpresa WHERE serie = '" . $data['serie'] . "';";
 		$previous = $this->connector->query($query);
 		if (count($previous) == 0) {
 			$query = "INSERT INTO medidores SET serie = :serie, diametro = :diametro, modelo = :modelo, digitosLectura = :digitosLectura, empresaId = :empresaId, marca = :marca, fechaAsignacion = :fechaAsignacion;";
