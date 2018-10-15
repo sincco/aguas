595097afd0562c5b82bd3ab897f02ea775170f4c
Iván Miranda <ivan.miranda@sincco.com>
2018-10-14 20:49:48 -0500

    Fotos

--- subirfotos/index.php
+++ subirfotos/index.php
@@ -77,7 +77,7 @@ function resize($newWidth, $targetFile, $originalFile) {
         else if (move_uploaded_file($_FILES['attachments']['tmp_name'][0], $targetFile)) {
           $ext = explode('.', $targetFile);
           $ext = end($ext);
-          $destiny = pathinfo($targetFile, PATHINFO_DIRNAME) . '/' . pathinfo($targetFile, PATHINFO_BASENAME) . '_MIN.' . pathinfo($targetFile, PATHINFO_EXTENSION);
+          $destiny = pathinfo($targetFile, PATHINFO_DIRNAME) . '/' . str_replace("." . pathinfo($targetFile, PATHINFO_EXTENSION), "", pathinfo($targetFile, PATHINFO_BASENAME)) . '_MIN.' . pathinfo($targetFile, PATHINFO_EXTENSION);
           // resize(900, $destiny, $name);
           $msg = array("status" => 1, "msg" => "File Has Been Uploaded", "path" => "../_expedientes/" . $dir[0] . "/" . basename($_FILES['attachments']['name'][0]), "new"=>$destiny);
         }
