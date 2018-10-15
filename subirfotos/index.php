982c7c51d729cc13a216334a736700e3ef2083e0
Iván Miranda <ivan.miranda@sincco.com>
2018-10-14 20:14:07 -0500

    Fotos

--- subirfotos/index.php
+++ subirfotos/index.php
@@ -1,13 +1,16 @@
 <?php
     if (isset($_FILES['attachments'])) {
-        #$msg = "";
-        #$targetFile = "uploads/" . basename($_FILES['attachments']['name'][0]);
-        #if (file_exists($targetFile))
-        #    $msg = array("status" => 0, "msg" => "File already exists!");
-        #else if (move_uploaded_file($_FILES['attachments']['tmp_name'][0], $targetFile))
-        #    $msg = array("status" => 1, "msg" => "File Has Been Uploaded", "path" => $targetFile);
-        #
-        $msg = $_FILES;
+        $msg = "";
+        $dir = explode("_", $_FILES['attachments']['name'][0]);
+        $_FILES['attachments']['name'][0] = str_replace($dir[0] . "_", "", $_FILES['attachments']['name'][0]);
+        $targetFile = ".._expedientes/" . $dir[0] . basename($_FILES['attachments']['name'][0]);
+        #if (file_exists($targetFile)) {
+        #  $msg = array("status" => 0, "msg" => "File already exists!");
+        #}
+        #else if (move_uploaded_file($_FILES['attachments']['tmp_name'][0], $targetFile)) {
+          $msg = array("status" => 1, "msg" => "File Has Been Uploaded", "path" => $targetFile);
+        #}
+        # $msg = $_FILES;
         exit(json_encode($msg));
     }
 ?>
