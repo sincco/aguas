2bff27dba41302c3796c6ddbb39e19219c465423
Iván Miranda <ivan.miranda@sincco.com>
2018-10-14 20:43:17 -0500

    Fotos

--- subirfotos/index.php
+++ subirfotos/index.php
@@ -1,4 +1,68 @@
 <?php
+
+private function resize($newWidth, $targetFile, $originalFile) {
+    $date = date("d M Y H:i:s.", filectime($originalFile));
+    $info = getimagesize($originalFile);
+    $mime = $info['mime'];
+
+    switch ($mime) {
+      case 'image/jpeg':
+        $image_create_func = 'imagecreatefromjpeg';
+        $image_save_func = 'imagejpeg';
+        $new_image_ext = 'jpg';
+        break;
+
+      case 'image/png':
+        $image_create_func = 'imagecreatefrompng';
+        $image_save_func = 'imagepng';
+        $new_image_ext = 'png';
+        break;
+
+      case 'image/gif':
+        $image_create_func = 'imagecreatefromgif';
+        $image_save_func = 'imagegif';
+        $new_image_ext = 'gif';
+        break;
+
+      default: 
+      throw new Exception('Unknown image type.');
+    }
+
+    $img = $image_create_func($originalFile);
+    list($width, $height) = getimagesize($originalFile);
+
+    $newHeight = ($height / $width) * $newWidth;
+    $tmp = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($tmp, $img, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+    if (file_exists($targetFile)) {
+      unlink($targetFile);
+    }
+    $image_save_func($tmp, $targetFile);
+    $this->watermark($targetFile, $image_create_func, $image_save_func, $date);
+  }
+
+  private function watermark($fileName, $image_create_func, $image_save_func, $text = false) {
+    $im = $image_create_func($fileName);
+    $estampa = imagecreatefrompng('html/img/adp_watermark.png');
+
+    $margen_dcho = 10;
+    $margen_inf = 10;
+    $sx = imagesx($estampa);
+    $sy = imagesy($estampa);
+
+    imagecopy($im, $estampa, imagesx($im) - $sx - $margen_dcho, imagesy($im) - $sy - $margen_inf, 0, 0, imagesx($estampa), imagesy($estampa));
+    if ($text) {
+      $text_color = imagecolorallocate($im, 0, 0, 0);
+      imagestring($im, 5, 5, 5,  $text, $text_color);
+      $text_color = imagecolorallocate($im, 255, 248, 6);
+      imagestring($im, 5, 4, 4,  $text, $text_color);
+    }
+
+    $image_save_func($im, $fileName);
+    imagedestroy($im);
+  }
+
     if (isset($_FILES['attachments'])) {
         $msg = "";
         $dir = explode("_", $_FILES['attachments']['name'][0]);
@@ -11,7 +75,11 @@
           $msg = array("status" => 0, "msg" => "File already exists!");
         }
         else if (move_uploaded_file($_FILES['attachments']['tmp_name'][0], $targetFile)) {
-          $msg = array("status" => 1, "msg" => "File Has Been Uploaded", "path" => "../_expedientes/" . $dir[0] . "/" . basename($_FILES['attachments']['name'][0]));
+          $ext = explode('.', $targetFile);
+          $ext = end($ext);
+          $destiny = pathinfo($targetFile, PATHINFO_DIRNAME) . '/' . pathinfo($targetFile, PATHINFO_BASENAME) . '_MIN.' . pathinfo($targetFile, PATHINFO_EXTENSION);
+          // resize(900, $destiny, $name);
+          $msg = array("status" => 1, "msg" => "File Has Been Uploaded", "path" => "../_expedientes/" . $dir[0] . "/" . basename($_FILES['attachments']['name'][0]), "new"=>$destiny);
         }
         exit(json_encode($msg));
     }
